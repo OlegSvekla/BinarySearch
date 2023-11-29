a8837141d5d5882b8d1be00de86ccedfe72c42a2
OlegSvekla <olegsvekla993@gmail.com>
2023-11-29 14:17:22 +0300

    add Search method into main method using binary search

---
+++ BinarySearch/BinarySearch/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿public class BinarySearch
+{
+    public static void Main()
+    {
+        int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = 73;
+
+        int result = Search(sortedArray, target);
+        if (result != -1)
+            Console.WriteLine($"Element {target} found by index {result}.");
+
+        else
+            Console.WriteLine($"-1");
+    }
+
+    public static int Search(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+                return mid;
+
+            else if (array[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
